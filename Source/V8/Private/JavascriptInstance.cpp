9ae3a6b3ed2001cb1afde61524ec79a0f521c420
getnamo <getnamo@gmail.com>
2020-05-23 20:18:19 -0700

    js instance work

--- Source/V8/Private/JavascriptInstance.cpp
+++ Source/V8/Private/JavascriptInstance.cpp
@@ -1,11 +1,92 @@
-
-
 #include "JavascriptInstance.h"
 
-FJavascriptInstance::FJavascriptInstance()
+#include "JavascriptIsolate_Private.h"
+#include "JavascriptContext_Private.h"
+
+FJavascriptFeatures::FJavascriptFeatures()
 {
+	ClearFeatures();
+}
+
+void FJavascriptFeatures::AddDefaultIsolateFeatures()
+{
+	FeatureMap.Add(TEXT("UnrealClasses"), TEXT("default"));
+	FeatureMap.Add(TEXT("UnrealMemory"), TEXT("default"));
+	FeatureMap.Add(TEXT("UnrealGlobals"), TEXT("default"));
+	FeatureMap.Add(TEXT("UnrealMisc"), TEXT("default"));
+}
+
+void FJavascriptFeatures::AddDefaultContextFeatures()
+{
+	FeatureMap.Add(TEXT("Root"), TEXT("default"));
+	FeatureMap.Add(TEXT("World"), TEXT("default"));
+	FeatureMap.Add(TEXT("Engine"), TEXT("default"));
+
+	FeatureMap.Add(TEXT("Context"), TEXT("default"));
+}
+
+void FJavascriptFeatures::ClearFeatures()
+{
+	FeatureMap.Empty();
+}
+
+FJavascriptInstance::FJavascriptInstance(const FJavascriptFeatures& InFeatures, TSharedPtr<FJavascriptIsolate> TargetIsolate /*= nullptr*/, FString TargetDomain /*= TEXT("default")*/)
+{
+	//Todo: initialize on bg thread option
+	Features = InFeatures;
+
+	bIsolateIsUnique = true;
+	IsolateDomain = TargetDomain;
+	//todo use domain to fetch same isolates from stack
+
+	//Create Isolate unless passed in
+	if (TargetIsolate.IsValid())
+	{
+		Isolate = TargetIsolate;
+		bIsolateIsUnique = false;
+	}
+	else
+	{
+		Isolate = TSharedPtr<FJavascriptIsolate>(FJavascriptIsolate::Create(false));
+		Isolate->SetAvailableFeatures(Features.FeatureMap);
+	}
+
+	//Create context on Isolate
+	Context = TSharedPtr<FJavascriptContext>(FJavascriptContext::Create(Isolate, Paths));
+	if (Features.FeatureMap.Contains(TEXT("Context")))
+	{
+		//Can't be exposed in instance variant
+		//Context->Expose("Context", this);
+	}
+
+	if (Features.FeatureMap.Contains(TEXT("UnrealGlobals")))
+	{
+		Context->ExposeGlobals();
+	}
+	if (Features.FeatureMap.Contains(TEXT("Engine")))
+	{
+		Context->Expose("GEngine", GEngine);
+	}
+
+	//World and Root need to be expose in a UObject
+
+	if (bIsolateIsUnique)
+	{
+		//Todo: manage the thread details for the isolate, since this instance is the creator of the isolate.
+	}
 }
 
 FJavascriptInstance::~FJavascriptInstance()
 {
+	Context.Reset();
+	if (bIsolateIsUnique)
+	{
+		Isolate.Reset();
+	}
 }
+
+TSharedPtr<FJavascriptIsolate> FJavascriptInstance::GetSharedIsolate()
+{
+	return Isolate;
+}
+
--- Source/V8/Public/JavascriptInstance.h
+++ Source/V8/Public/JavascriptInstance.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "JavascriptIsolate.h"
+#include "JavascriptContext.h"
 #include "JavascriptInstance.generated.h"
 
 USTRUCT()
@@ -27,6 +29,22 @@ struct V8_API FJavascriptClassAsset
 	TSubclassOf<UObject> Class;
 };
 
+USTRUCT(BlueprintType)
+struct V8_API FJavascriptFeatures
+{
+	GENERATED_USTRUCT_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Javascript")
+	TMap<FString, FString> FeatureMap;
+
+	FJavascriptFeatures();
+
+	//C++ functions
+	void AddDefaultIsolateFeatures();
+	void AddDefaultContextFeatures();
+	void ClearFeatures();
+};
+
 /**
  * Holds a unreal wrapped context + features running on specified isolates.
  * Refactored to be main wrapper for a JS instance. Isolates are root attached
@@ -34,6 +52,23 @@ struct V8_API FJavascriptClassAsset
 class V8_API FJavascriptInstance
 {
 public:
-	FJavascriptInstance();
+	FJavascriptInstance(const FJavascriptFeatures& InFeatures, TSharedPtr<FJavascriptIsolate> TargetIsolate = nullptr, FString TargetDomain = TEXT("default"));
 	~FJavascriptInstance();
+
+	/** To re-use an isolated, grab a shared pointer and pass it into another instance */
+	TSharedPtr<FJavascriptIsolate> GetSharedIsolate();
+
+protected:
+	/** This specifies the available exposures to the context */
+	FJavascriptFeatures Features;
+	bool bIsolateIsUnique;
+
+	//Thread/threadid?
+	//Should the isolates be thread managed here or a different class?
+	FString IsolateDomain;
+
+	TSharedPtr<FJavascriptIsolate> Isolate;
+	TSharedPtr<FJavascriptContext> Context;
+	TArray<FString> Paths;
+	TSharedPtr<FString> ContextId;
 };
